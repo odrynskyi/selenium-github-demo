69648e65e14e6eb75281d8e2aa14d6620105e2d3
oleksandr.odrynskyi <oleksandr.odrynskyi@globallogic.com>
2017-11-14 12:35:00 +0200

    Home task 7. Done! Final version.

--- src/test/java/Task07.java
+++ src/test/java/Task07.java
@@ -9,54 +9,61 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class Task07 {
 
-    public static WebDriver chromeDriver;
+    public static WebDriver driver;
+    private static WebDriverWait wait;
     private static boolean isElementPresent(By locator) {
-        return chromeDriver.findElements(locator).size() > 0;
+        return driver.findElements(locator).size() > 0;
     }
+
     @BeforeClass
     public static void start(){
         ChromeDriverManager.getInstance().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("start-maximized");
-        chromeDriver = new ChromeDriver(chromeOptions);
-        chromeDriver.get("http://localhost/litecart/admin");
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, 10);
+        driver.get("http://localhost/litecart/admin");
         if(isElementPresent(By.name("login"))){
-            chromeDriver.findElement(By.name("username")).sendKeys("admin");
-            chromeDriver.findElement(By.name("password")).sendKeys("admin");
-            chromeDriver.findElement(By.name("login")).click();
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("login")).click();
         }
-        chromeDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        wait.until((WebDriver d) -> d.findElement(By.cssSelector("#sidebar")));
+    }
+
+    @AfterClass
+    public static void stop(){
+        driver.quit();
+        driver = null;
     }
 
     @Test
     public void isNewWindowOpened(){
-        chromeDriver.findElement(By.cssSelector("#box-apps-menu li:nth-child(3)")).click();
-        chromeDriver.findElement(By.cssSelector("#content div a")).click();
-        String originalWindow = chromeDriver.getWindowHandle();
+        driver.findElement(By.cssSelector("#box-apps-menu li:nth-child(3)")).click();
+        driver.findElement(By.cssSelector("#content div a")).click();
+        String originalWindow = driver.getWindowHandle();
 
-        List<WebElement> itemsList = chromeDriver.findElements(By.cssSelector("i[class='fa fa-external-link']"));
+        List<WebElement> itemsList = driver.findElements(By.cssSelector("i[class='fa fa-external-link']"));
         for (int i=0; i<itemsList.size(); i++) {
-            Set<String> existWindows = chromeDriver.getWindowHandles();
-            WebDriverWait wait = new WebDriverWait(chromeDriver, 10);
+            Set<String> existWindows = driver.getWindowHandles();
+            WebDriverWait wait = new WebDriverWait(driver, 10);
             itemsList.get(i).click();
             String newWindow = wait.until(anyWindowOtherThan(existWindows));
-            chromeDriver.switchTo().window(newWindow);
-            chromeDriver.close();
-            chromeDriver.switchTo().window(originalWindow);
+            driver.switchTo().window(newWindow);
+            driver.close();
+            driver.switchTo().window(originalWindow);
         }
     }
 
     private ExpectedCondition<String> anyWindowOtherThan(Set<String> windows) {
         return new ExpectedCondition<String>() {
             public String apply(WebDriver input) {
-                Set<String> handles = chromeDriver.getWindowHandles();
+                Set<String> handles = driver.getWindowHandles();
                 handles.removeAll(windows);
                 return handles.size() > 0 ? handles.iterator().next() : null;
             }
