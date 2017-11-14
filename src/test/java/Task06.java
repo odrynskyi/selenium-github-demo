54c9c1320760153e911c4fbb3498bbb5d2b8b707
oleksandr.odrynskyi <oleksandr.odrynskyi@globallogic.com>
2017-11-14 12:26:05 +0200

    Home task 6. Done! Final version.

--- src/test/java/Task06.java
+++ src/test/java/Task06.java
@@ -18,56 +18,50 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Task06 {
-    public static WebDriver chromeDriver;
+    public static WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeClass
     public static void start(){
         ChromeDriverManager.getInstance().setup();
-        //ChromeOptions chromeOptions = new ChromeOptions();
-        //chromeOptions.addArguments("start-maximized");
-        chromeDriver = new ChromeDriver();
-        chromeDriver.get("http://localhost/litecart");
-        chromeDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+        wait = new WebDriverWait(driver, 10);
+        driver.get("http://localhost/litecart");
+        wait.until((WebDriver d) -> d.findElement(By.cssSelector("img[title='My Store']")));
     }
 
     @AfterClass
     public static void stop(){
-        chromeDriver.quit();
-        chromeDriver = null;
+        driver.quit();
+        driver = null;
     }
 
     @Test
     public void AddAndRemoveProducts(){
-        WebDriverWait wait = new WebDriverWait(chromeDriver,10);
+        WebDriverWait wait = new WebDriverWait(driver,10);
 
         for (int i = 0; i < 4; i++){
-            chromeDriver.findElement(By.cssSelector("div#box-most-popular div ul li:nth-child(" + (i+1) +")")).click();
+            driver.findElement(By.cssSelector("div#box-most-popular div ul li:nth-child(" + (i+1) +")")).click();
 
-            if (chromeDriver.findElements(By.cssSelector("select[name='options[Size]']")).size() > 0) {
-            Select purchaseCurrencyCode = new Select (chromeDriver.findElement(By.name("options[Size]")));
+            if (driver.findElements(By.cssSelector("select[name='options[Size]']")).size() > 0) {
+            Select purchaseCurrencyCode = new Select (driver.findElement(By.name("options[Size]")));
             purchaseCurrencyCode.selectByValue("Small");}
-            Integer currentProductNumber = Integer.valueOf(chromeDriver.findElement(By.cssSelector("span.quantity")).getText());
-            chromeDriver.findElement(By.name("add_cart_product")).click();
+            Integer currentProductNumber = Integer.valueOf(driver.findElement(By.cssSelector("span.quantity")).getText());
+            driver.findElement(By.name("add_cart_product")).click();
             wait.until(ExpectedConditions.textToBe(By.cssSelector("span.quantity"), String.valueOf(currentProductNumber + 1)));
-            chromeDriver.get("http://localhost/litecart");
+            driver.get("http://localhost/litecart");
         }
-        chromeDriver.findElement(By.cssSelector("div#cart a.link")).click();
-
-        while (chromeDriver.findElements(By.cssSelector("table[class='dataTable rounded-corners']")).size() > 0) {
-            if (chromeDriver.findElements(By.cssSelector("li.shortcut")).size() > 0){
-                chromeDriver.findElement(By.cssSelector("li.shortcut")).click();}
-                WebElement isTablePresent = chromeDriver.findElement(By.cssSelector("table[class='dataTable rounded-corners']"));
-                chromeDriver.findElement(By.name("remove_cart_item")).click();
+        driver.findElement(By.cssSelector("div#cart a.link")).click();
+        while (driver.findElements(By.cssSelector("table[class='dataTable rounded-corners']")).size() > 0) {
+            if (driver.findElements(By.cssSelector("li.shortcut")).size() > 0){
+                driver.findElement(By.cssSelector("li.shortcut")).click();}
+                WebElement isTablePresent = driver.findElement(By.cssSelector("table[class='dataTable rounded-corners']"));
+                driver.findElement(By.name("remove_cart_item")).click();
                 wait.until(ExpectedConditions.stalenessOf(isTablePresent));
             }
 
-        Assert.assertTrue("False",chromeDriver.findElements(By.cssSelector("table[class='dataTable rounded-corners']")).size() == 0);
-
-
-
-
+        Assert.assertTrue("Product table is still present but should be removed.",driver.findElements(By.cssSelector("table[class='dataTable rounded-corners']")).size() == 0);
     }
-
-
-
 }
