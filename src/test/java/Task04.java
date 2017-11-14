924c75fe002dbaf07e7d621f2bb6b30f7d138e1e
oleksandr.odrynskyi <oleksandr.odrynskyi@globallogic.com>
2017-11-14 17:28:29 +0200

    Home task 9. Done!

--- src/test/java/Task04.java
+++ src/test/java/Task04.java
@@ -2,6 +2,8 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,132 +13,101 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import java.util.concurrent.TimeUnit;
 
+@RunWith(Parameterized.class)
 public class Task04{
-    public static WebDriver chromeDriver;
-    public static WebDriver firefoxDriver;
-    public static WebDriver iExplorerDriver;
 
-    @BeforeClass
-    public static void start(){
-        ChromeDriverManager.getInstance().setup(); // Chrome 62.0
-        FirefoxDriverManager.getInstance().setup(); // FF 56.0
-        InternetExplorerDriverManager.getInstance().arch32().setup(); //IE 11.0
+    @Parameterized.Parameter
+    public static WebDriver driver;
+//    public static WebDriver firefoxDriver;
+//    public static WebDriver iExplorerDriver;
+    String baseURL = "http://localhost/litecart";
+
+    @Before
+    public void start(){
+        driver.get("http://localhost/litecart/en/");
     }
 
     @AfterClass
     public static void stop(){
-        iExplorerDriver = null;
-        firefoxDriver = null;
-        chromeDriver = null;
+        driver.quit();
     }
 
     @Test
-    public void CheckCampaignElementsInChromeBrowser(){
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("start-maximized");
-        chromeDriver = new ChromeDriver(chromeOptions);
-        chromeDriver.get("http://localhost/litecart");
-        WebElement productNameMainLocator = chromeDriver.findElement(By.cssSelector("div#box-campaigns .name"));
+    public void CheckProductName(){
+        WebElement productNameMainLocator = driver.findElement(By.cssSelector("div#box-campaigns .name"));
         String productNameMain = productNameMainLocator.getAttribute("textContent");
-        WebElement salePriceLocator = chromeDriver.findElement(By.cssSelector("div#box-campaigns .campaign-price"));
-        String salePriceMain = salePriceLocator.getAttribute("textContent");
-        String salePriceColorMain = salePriceLocator.getCssValue("color");
-        WebElement regularPriceMainLocator = chromeDriver.findElement(By.cssSelector("div#box-campaigns .regular-price"));
-        String regularPriceMain = regularPriceMainLocator.getAttribute("textContent");
-        String regularPriceColorMain = regularPriceMainLocator.getCssValue("color");
-        String regularPriceTextStrikeMain = regularPriceMainLocator.getCssValue("text-decoration");
         productNameMainLocator.click();
-        chromeDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        WebElement productNameLocatorPage = chromeDriver.findElement(By.cssSelector(".content h1"));
+        WebElement productNameLocatorPage = driver.findElement(By.cssSelector(".content h1"));
         String productNamePage = productNameLocatorPage.getAttribute("textContent");
-        WebElement salePriceLocatorPage = chromeDriver.findElement(By.cssSelector(".campaign-price "));
-        String salePricePage = salePriceLocatorPage.getAttribute("textContent");
-        String salePriceColorPage = salePriceLocatorPage.getCssValue("color");
-        WebElement regularPriceLocatorPage = chromeDriver.findElement(By.cssSelector(".regular-price"));
-        String regularPricePage = regularPriceLocatorPage.getAttribute("textContent");
-        String regularPriceColorPage = regularPriceLocatorPage.getCssValue("color");
-        String regularPriceTextStrikePage = regularPriceLocatorPage.getCssValue("text-decoration");
 
         Assert.assertEquals("Product Name on the main page differs from Product Name on the product page.", productNameMain, productNamePage);
-        Assert.assertEquals("Sale Price on the main page differs from Sale Price on the product page.", salePriceMain, salePricePage);
-        Assert.assertEquals("Regular Price on the main page differs from Regular Price on the product page.", regularPriceMain, regularPricePage);
-        Assert.assertEquals("Sale Price color on the main page differs from Sale Price color on the product page.", salePriceColorMain, salePriceColorPage);
-        Assert.assertEquals("Regular Price color on the Main page differs from expected.", regularPriceColorMain, "rgba(119, 119, 119, 1)");
-        Assert.assertEquals("Regular Price color on the Product page differs from expected.", regularPriceColorPage, "rgba(102, 102, 102, 1)");
-        Assert.assertEquals("Regular Price strike on the Main page differs from expected.", regularPriceTextStrikeMain, "line-through solid rgb(119, 119, 119)");
-        Assert.assertEquals("Regular Price strike on the Product page differs from expected.", regularPriceTextStrikePage, "line-through solid rgb(102, 102, 102)");
-        chromeDriver.quit();
     }
 
     @Test
-    public void CheckCampaignElementsInIexplorerBrowser(){
-        iExplorerDriver = new InternetExplorerDriver();
-        iExplorerDriver.get("http://localhost/litecart");
-        WebElement productNameMainLocator = iExplorerDriver.findElement(By.cssSelector("div#box-campaigns .name"));
-        String productNameMain = productNameMainLocator.getAttribute("textContent");
-        WebElement salePriceLocator = iExplorerDriver.findElement(By.cssSelector("div#box-campaigns .campaign-price"));
+    public void CheckDiscountPrice() {
+        WebElement salePriceLocator = driver.findElement(By.cssSelector("div#box-campaigns .campaign-price"));
         String salePriceMain = salePriceLocator.getAttribute("textContent");
-        String salePriceColorMain = salePriceLocator.getCssValue("color");
-        WebElement regularPriceMainLocator = iExplorerDriver.findElement(By.cssSelector("div#box-campaigns .regular-price"));
-        String regularPriceMain = regularPriceMainLocator.getAttribute("textContent");
-        String regularPriceColorMain = regularPriceMainLocator.getCssValue("color");
-        String regularPriceTextStrikeMain = regularPriceMainLocator.getCssValue("text-decoration");
-        productNameMainLocator.click();
-        iExplorerDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        WebElement productNameLocatorPage = iExplorerDriver.findElement(By.cssSelector(".content h1"));
-        String productNamePage = productNameLocatorPage.getAttribute("textContent");
-        WebElement salePriceLocatorPage = iExplorerDriver.findElement(By.cssSelector(".campaign-price "));
+        ClickCampaignProduct();
+        WebElement salePriceLocatorPage = driver.findElement(By.cssSelector(".campaign-price "));
         String salePricePage = salePriceLocatorPage.getAttribute("textContent");
-        String salePriceColorPage = salePriceLocatorPage.getCssValue("color");
-        WebElement regularPriceLocatorPage = iExplorerDriver.findElement(By.cssSelector(".regular-price"));
-        String regularPricePage = regularPriceLocatorPage.getAttribute("textContent");
-        String regularPriceColorPage = regularPriceLocatorPage.getCssValue("color");
-        String regularPriceTextStrikePage = regularPriceLocatorPage.getCssValue("text-decoration");
 
-        Assert.assertEquals("Product Name on the main page differs from Product Name on the product page.", productNameMain, productNamePage);
         Assert.assertEquals("Sale Price on the main page differs from Sale Price on the product page.", salePriceMain, salePricePage);
-        Assert.assertEquals("Regular Price on the main page differs from Regular Price on the product page.", regularPriceMain, regularPricePage);
-        Assert.assertEquals("Sale Price color on the main page differs from Sale Price color on the product page.", salePriceColorMain, salePriceColorPage);
-        Assert.assertEquals("Regular Price color on the Main page differs from expected.",  "rgba(119, 119, 119, 1)", regularPriceColorMain);
-        Assert.assertEquals("Regular Price color on the Product page differs from expected.", "rgba(102, 102, 102, 1)", regularPriceColorPage);
-        Assert.assertEquals("Regular Price strike on the Product page differs from expected.", regularPriceTextStrikeMain, regularPriceTextStrikePage);
-        iExplorerDriver.quit();
     }
 
     @Test
-    public void CheckCampaignElementsInFirefoxBrowser() {
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.addArguments("start-maximized");
-        firefoxDriver = new FirefoxDriver(firefoxOptions);
-        firefoxDriver.get("http://localhost/litecart");
-        WebElement productNameMainLocator = firefoxDriver.findElement(By.cssSelector("div#box-campaigns .name"));
-        String productNameMain = productNameMainLocator.getAttribute("textContent");
-        WebElement salePriceLocator = firefoxDriver.findElement(By.cssSelector("div#box-campaigns .campaign-price"));
-        String salePriceMain = salePriceLocator.getAttribute("textContent");
-        String salePriceColorMain = salePriceLocator.getCssValue("color");
-        WebElement regularPriceMainLocator = firefoxDriver.findElement(By.cssSelector("div#box-campaigns .regular-price"));
-        String regularPriceMain = regularPriceMainLocator.getAttribute("textContent");
+    public void CheckRegularPriceColor() {
+        WebElement regularPriceMainLocator = driver.findElement(By.cssSelector("div#box-campaigns .regular-price"));
         String regularPriceColorMain = regularPriceMainLocator.getCssValue("color");
-        String regularPriceTextStrikeMain = regularPriceMainLocator.getCssValue("text-decoration");
-        productNameMainLocator.click();
-        firefoxDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        WebElement productNameLocatorPage = firefoxDriver.findElement(By.cssSelector(".content h1"));
-        String productNamePage = productNameLocatorPage.getAttribute("textContent");
-        WebElement salePriceLocatorPage = firefoxDriver.findElement(By.cssSelector(".campaign-price "));
-        String salePricePage = salePriceLocatorPage.getAttribute("textContent");
-        String salePriceColorPage = salePriceLocatorPage.getCssValue("color");
-        WebElement regularPriceLocatorPage = firefoxDriver.findElement(By.cssSelector(".regular-price"));
-        String regularPricePage = regularPriceLocatorPage.getAttribute("textContent");
+        ClickCampaignProduct();
+        WebElement regularPriceLocatorPage = driver.findElement(By.cssSelector(".regular-price"));
         String regularPriceColorPage = regularPriceLocatorPage.getCssValue("color");
-        String regularPriceTextStrikePage = regularPriceLocatorPage.getCssValue("text-decoration");
-        firefoxDriver.quit();
+        if(driver instanceof FirefoxDriver){
+            Assert.assertEquals("Regular Price color on the Main page differs from expected.", "rgb(119, 119, 119)", regularPriceColorMain);
+            Assert.assertEquals("Regular Price color on the Product page differs from expected.", "rgb(102, 102, 102)", regularPriceColorPage );
+        } else {
+            Assert.assertEquals("Regular Price color on the Main page differs from expected.", "rgba(119, 119, 119, 1)", regularPriceColorMain);
+            Assert.assertEquals("Regular Price color on the Product page differs from expected.", "rgba(102, 102, 102, 1)", regularPriceColorPage);
+        }
+    }
 
-        Assert.assertEquals("Product Name on the main page differs from Product Name on the product page.", productNameMain, productNamePage);
-        Assert.assertEquals("Sale Price on the main page differs from Sale Price on the product page.", salePriceMain, salePricePage);
-        Assert.assertEquals("Regular Price on the main page differs from Regular Price on the product page.", regularPriceMain, regularPricePage);
+    @Test
+    public void CheckRegularPriceStrike() {
+        WebElement regularPriceMainLocator = driver.findElement(By.cssSelector("div#box-campaigns .regular-price"));
+        String regularPriceTextStrikeMain = regularPriceMainLocator.getCssValue("text-decoration");
+        ClickCampaignProduct();
+        WebElement regularPriceLocatorPage = driver.findElement(By.cssSelector(".regular-price"));
+        String regularPriceTextStrikePage = regularPriceLocatorPage.getCssValue("text-decoration");
+        if(driver instanceof ChromeDriver){
+            Assert.assertEquals("Regular Price strike on the Main page differs from expected.", "line-through solid rgb(119, 119, 119)", regularPriceTextStrikeMain);
+            Assert.assertEquals("Regular Price strike on the Main page differs from expected.", "line-through solid rgb(102, 102, 102)", regularPriceTextStrikePage);
+        }else{
+        Assert.assertEquals("Regular Price strike on the Main page differs from expected.", regularPriceTextStrikeMain, regularPriceTextStrikePage);}
+    }
+
+    @Test
+    public void CheckDiscountPriceColor() {
+        WebElement salePriceLocator = driver.findElement(By.cssSelector("div#box-campaigns .campaign-price"));
+        String salePriceColorMain = salePriceLocator.getCssValue("color");
+        ClickCampaignProduct();
+        WebElement salePriceLocatorPage = driver.findElement(By.cssSelector(".campaign-price "));
+        String salePriceColorPage = salePriceLocatorPage.getCssValue("color");
+
         Assert.assertEquals("Sale Price color on the main page differs from Sale Price color on the product page.", salePriceColorMain, salePriceColorPage);
-        Assert.assertEquals("Regular Price color on the Main page differs from expected.", "rgb(119, 119, 119)", regularPriceColorMain);
-        Assert.assertEquals("Regular Price color on the Product page differs from expected.", "rgb(102, 102, 102)", regularPriceColorPage );
-        Assert.assertEquals("Regular Price strike on the Main page differs from expected.", regularPriceTextStrikeMain, regularPriceTextStrikePage);
+    }
+
+    @Parameterized.Parameters
+    public static WebDriver[] getDriver(){
+        ChromeDriverManager.getInstance().setup();
+        FirefoxDriverManager.getInstance().setup();
+        InternetExplorerDriverManager.getInstance().arch32().setup();
+
+        FirefoxOptions fopt = new FirefoxOptions();
+        fopt.setCapability(FirefoxDriver.MARIONETTE, false);
+        return new WebDriver[]{new FirefoxDriver(), new ChromeDriver(), new InternetExplorerDriver()};
+    }
+
+    private void ClickCampaignProduct(){
+        WebElement productNameMainLocator = driver.findElement(By.cssSelector("div#box-campaigns .name"));
+        productNameMainLocator.click();
     }
 }
